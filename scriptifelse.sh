e3978090db1c6faf50aca6ab9e4ae6f95d5c5b8b
sreekanth <sreekanth@gmail.com>
2022-11-26 19:15:52 +0530

    script debug set -x

---
+++ scriptifelse.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+marks=$1
+if [ $marks -ge 80 ]; then
+    echo "Excellent"
+
+elif [ $marks -ge 60 ]; then
+    echo "Good"
+
+else
+    echo "Satisfactory"
+fi
+
+marks=$2
+if [ $marks -lt 100 ]; then
+    echo "greater than 100"
+elif [ $marks -ge 200]; then
+    echo "greater than 200"
+else
+    echo "else block"
+fi
