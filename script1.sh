e007c7b21123176ff08541112c2353625b280636
sreekanth <sreekanth@gmail.com>
2022-11-26 14:51:02 +0530

    script1.sh

--- script1.sh
+++ script1.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-#Example1
 A=10
 B=20
 C=30
@@ -7,8 +6,10 @@ echo  ${A} ${B} ${C}
 echo "--------------------------------------"
 
 echo "----------forloop----------------------------"
-REGIONS=$1
-for REGION in {REGIONS};do
+var1=$1
+var2=$2
+var3=$3
+for REGION in {1..5};do
 aws ec2 describe-subnets | jq ".Subnets[].AvailabilityZone"
-echo "Region ======     $REGIONS" 
+echo "Region ======  $REGION    $var1    $var2    $var3" 
 done
