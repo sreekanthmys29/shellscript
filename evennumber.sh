3c4778198cc2e1e3eb1a8580f2887f2d13b6e80e
sreekanth <sreekanth@gmail.com>
2022-11-26 21:39:30 +0530

    Even Number with Number Array Elements

--- evennumber.sh
+++ evennumber.sh
@@ -2,9 +2,9 @@
 set -x -e
 START=1
 END=100
-
+# ! -o -a
 for i in $(seq $START $END); do
-    if [ $(expr $i % 2) -eq 0 ]; then
+    if [ $i -gt 0 -a $(expr $i % 2) -eq 0 ]; then
         echo "print number is $i"
     fi
 done
