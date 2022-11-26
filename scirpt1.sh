72c7c87ef5a7c99d5be0144de254d346d2650112
sreekanth <sreekanth@gmail.com>
2022-11-26 14:04:31 +0530

    script1.sh

--- scirpt1.sh
+++ scirpt1.sh
@@ -1,1 +1,14 @@
 #!bin/bash
+#Example1
+A=10
+B=20
+C=30
+echo ${A} {B} {C}
+echo "--------------------------------------"
+
+echo "----------forloop----------------------------"
+REGIONS=$1
+FOR REGION IN {REGIONS};do
+aws ec2 describe-subnets | jq ".Subnets[].AvailabilityZone"
+echo "Region ======     $REGION" 
+done
