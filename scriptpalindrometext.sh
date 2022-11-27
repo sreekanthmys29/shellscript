b1009d04822e3b8c4e1bc94b8cb0de80de19d52f
Ubuntu <ubuntu@ip-172-31-14-136.ap-northeast-1.compute.internal>
2022-11-27 16:17:46 +0000

    shell palindrome text

---
+++ scriptpalindrometext.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo "test palindrome text"
+set -x -e
+INPUT="TENET"
+REVERSE=""
+length=${#INPUT}
+echo "Input value  $INPUT"
+for ((i = $length-1; i >= 0; i--)); do
+
+    REVERSE="$REVERSE${INPUT:$i:1}"
+done
+ echo $REVERSE
+if [ $INPUT = $REVERSE ]; then
+    echo "Input value  Palindrome"
+
+else
+    echo "Input value Is Not Palindrome"
+fi
